a4b64a9ee90040dff6c0ffd0f61b540ba847c313
IndianMorpheus <jyothish@juvenescence.ltd>
2020-02-13 13:24:52 +0000

    Stash changes

--- models/convolutional_vae.py
+++ models/convolutional_vae.py
@@ -6,17 +6,19 @@ from models.vae_template import VaeTemplate
 class ConvolutionalVAE(VaeTemplate):
     def __init__(self, model_config, h_dim, z_dim, out_dim, device):
         sizes: list = model_config["sizes"]
+        kernel_sizes: list = model_config["kernel_sizes"]
         encoder = nn.Sequential(
             nn.Conv1d(in_channels=sizes[0], out_channels=sizes[1], kernel_size=5, stride=1, padding=2, groups=1),
+            nn.BatchNorm1d(sizes[1]),
             nn.ReLU(),
             nn.Conv1d(in_channels=sizes[1], out_channels=sizes[2], kernel_size=5, stride=1, padding=2, groups=1),
+            nn.BatchNorm1d(sizes[2]),
             nn.ReLU(),
             nn.Conv1d(in_channels=sizes[2], out_channels=sizes[3], kernel_size=5, stride=1, padding=2, groups=1),
             nn.ReLU(),
+            nn.BatchNorm1d(sizes[3]),
             nn.Conv1d(in_channels=sizes[3], out_channels=1, kernel_size=5, stride=1, padding=2, groups=1),
             nn.ReLU()  # ,
-            # nn.LSTM()
-            # Flatten()
         )
 
         decoder = nn.Sequential(
@@ -25,9 +27,9 @@ class ConvolutionalVAE(VaeTemplate):
             # UnFlatten(size=h_dim),
             nn.ConvTranspose1d(1, sizes[3], kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
-            nn.ConvTranspose1d(sizes[3], sizes[2], kernel_size=5, stride=1, padding= 2),
+            nn.ConvTranspose1d(sizes[3], sizes[2], kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
-            nn.ConvTranspose1d(sizes[2], sizes[1], kernel_size=5, stride=1, padding= 2),
+            nn.ConvTranspose1d(sizes[2], sizes[1], kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             nn.ConvTranspose1d(sizes[1], out_dim, kernel_size=5, stride=1, padding=2),
             nn.Sigmoid()
--- utils/tuning.py
+++ utils/tuning.py
@@ -4,27 +4,10 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from ray import tune
-from sklearn.metrics import f1_score, roc_auc_score
 
 
 def tune_model(config):
     tune.track.init()
-
-    dataset = config.get('dataset')
-    data = config.get("data")
-    known_mask = data.y.cpu().numpy() != 0
-    layer_1 = config["layer_1"]
-    layer_2 = config["layer_2"]
-    if config.get("task") == 'telological':
-        model = TeleosGCN(dataset=dataset,
-                          data=data,
-                          layer_1_size=layer_1,
-                          layer_2_size=layer_2).to(config.get('device'))
-    else:
-        model = GCNModel(dataset=dataset,
-                         data=data,
-                         layer_1_size=layer_1,
-                         layer_2_size=layer_2).to(config.get('device'))
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"], weight_decay=5e-5)
 
@@ -35,16 +18,11 @@ def tune_model(config):
         model.train()
         optimizer.zero_grad()
         logits, latent_1, latent_2, latent_3 = model()
-        alpha = np.logical_and(config.get('train_mask'), known_mask)
-        imb_Wc = torch.bincount(config.get('data').y[alpha]).float().clamp(min=1e-10, max=1e10) / \
-                 config.get('data').y[alpha].shape[0]
-        weights = (1 / imb_Wc) / (sum(1 / imb_Wc))
 
         loss = F.cross_entropy(logits[alpha],
                                config.get('data').y[alpha],
                                weight=weights)
         _loss = loss.clone().detach().to("cpu").item()
-        tune.track.log(loss=_loss)
 
         if config.get("early_stopping_loss"):
             if _loss > loss_state:
@@ -60,38 +38,10 @@ def tune_model(config):
         model.eval()
         logits, latent_1, latent_2, latent_3 = model()
         accs, auroc_scores, f1_scores = [], [], []
-        s_logits = F.softmax(input=logits[:, 1:], dim=1)
 
-        for mask in [config.get('train_mask'), config.get('test_mask')]:
-            alpha = np.logical_and(mask, known_mask)
-            pred = logits[alpha].max(1)[1]
-
-            accs.append(pred.eq(config.get('data').y[alpha]).sum().item() / alpha.sum().item())
-
-            f1_scores.append(f1_score(y_true=config.get('data').y[alpha].to('cpu'),
-                                      y_pred=pred.to('cpu'),
-                                      average='macro'))
-
-            #  FIXME: Error occurs with fold_class labelset, due to it not being possible to represent all 508
-            #         classes inside each of the splits. Fix either by creating a pruning the data to consolidate
-            #         the sizes of y_true and y_score right before auroc calculation.
-            if config.get("task") == 'binary':
-                auroc_scores.append(roc_auc_score(y_true=config.get('data').y[alpha].to('cpu').numpy(),
-                                                  y_score=np.amax(s_logits[alpha].to('cpu').data.numpy(), axis=1),
-                                                  average=config.get('auroc_average'),
-                                                  multi_class=None))
-            else:
-                auroc_scores.append(roc_auc_score(y_true=config.get('data').y[alpha].to('cpu').numpy(),
-                                                  y_score=s_logits[alpha].to('cpu').data.numpy(),
-                                                  average=config.get('auroc_average'),
-                                                  multi_class=config.get('auroc_versus')))
 
         train_acc, test_acc = accs
-        train_f1, test_f1 = f1_scores
-        train_auc, test_auc = auroc_scores
         tune.track.log(train_accuracy=train_acc, test_accuracy=test_acc)
-        tune.track.log(train_f1_score=train_f1, test_f1_score=test_f1)
-        tune.track.log(train_f1_score=train_auc, test_f1_score=test_auc)
 
         if config.get('early_stopping_accs'):
             if train_acc > accs_state:
@@ -105,7 +55,6 @@ def tune_model(config):
 
 class Tuner:
     def __init__(self, data_set, task, masks: list, tuning_config, device):
-
         train, test = masks
 
         tuning_config["train_mask"] = train
@@ -124,7 +73,6 @@ class Tuner:
 
     def run_tune(self):
         tuning_run_name = "tuning_quaternary"
-        logging_run_name = "quaternary_experiment"
         import multiprocessing
 
         cpus = int(multiprocessing.cpu_count())
@@ -146,4 +94,4 @@ class Tuner:
 
         df.to_csv(path_or_buf=osp.join("..", "logs", logging_run_name))
 
-        return analysis.get_best_config(metric="train_f1")  # needs to return config for best model
+        return analysis.get_best_config(metric="train_accuracy")  # needs to return config for best model
