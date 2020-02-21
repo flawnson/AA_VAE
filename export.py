0745dc58ff403519bdf22c679bca72842810de70
JyothishSoman <jyothish@juvenescence.ltd>
2020-02-21 12:16:51 +0000

    Added the model evaluation code.

--- export.py
+++ export.py
@@ -1,32 +1,46 @@
 """ Script used to export embeddings of proteins
 """
 import torch
-import data
 import pandas as pd
-FIXED_PROTEIN_LENGTH = 50
 
-
-""" Load the proteins
-"""
-
-protein_file = "data/human_proteins.json"
-proteins = pd.read_json(protein_file)
-proteins_onehot = data.read_sequences(protein_file, FIXED_PROTEIN_LENGTH, add_chemical_features=False)
+import argparse
+import json
+from utils import data
 
 """
 Load the saved model
 """
 from models.simple_vae import VAE
-EMBEDDING_LENGTH = 20
-INPUT_DIM = FIXED_PROTEIN_LENGTH * data.VOCABULARY_SIZE
-model_to_load = "simple_vae_02_05_2020_15_53_09"
-model = VAE(INPUT_DIM, EMBEDDING_LENGTH)
-model.load_state_dict(torch.load(f"saved_models/{model_to_load}"))
-model.eval()
+from models.convolutional_vae import ConvolutionalVAE
 
-"""
-Generate embeddings
-"""
-protein_embeddings = model.encode(proteins_onehot.view(-1, INPUT_DIM))
-proteins['embeddings'] = list(protein_embeddings.detach().numpy())
-proteins.to_json("exports/embeddings.json")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Config file parser")
+    parser.add_argument("-c", "--config", help="common config file", type=str)
+    parser.add_argument("-m", "--modelconfig", help="model config file", type=str)
+    parser.add_argument("-x", "--model", help="model to load", type=str)
+    args = parser.parse_args()
+    device = torch.device('cpu')
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    config: dict = json.load(open(args.config))
+    model_config: dict = json.load(open(args.modelconfig))
+    print(f"Creating the model")
+    if model_config["model_name"] == "convolutional_vae":
+        model = ConvolutionalVAE(model_config["convolutional_parameters"], config["hidden_size"],
+                                 config["embedding_size"], config["feature_length"], device,
+                                 data.get_embedding_matrix())
+    else:
+        model = VAE(1500, 20).to(device)  # 20 is number of hidden dimensio
+
+    FIXED_PROTEIN_LENGTH = config["protein_length"]
+    protein_file = "data/human_proteins.json"
+    proteins = pd.read_json(protein_file)
+    proteins_onehot = data.read_sequences(protein_file, FIXED_PROTEIN_LENGTH, add_chemical_features=False,
+                        sequence_only = True)
+    model_to_load = args.model
+    model.load_state_dict(torch.load(f"saved_models/{model_to_load}"))
+    model.eval()
+
+    protein_embeddings = model.representation(proteins_onehot)
+    proteins['embeddings'] = list(protein_embeddings.detach().numpy())
+    proteins.to_json("exports/embeddings.json")
