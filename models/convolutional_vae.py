3d2e713c65f9b4f2f1071db3e1f287fbabf4b1a0
JyothishSoman <jyothish@juvenescence.ltd>
2020-02-24 15:06:21 +0000

    Formatted code.

--- models/convolutional_vae.py
+++ models/convolutional_vae.py
@@ -11,8 +11,10 @@ def init_weights(m):
 def out_size_conv(current_layer, padding, dilation, kernel_size, stride):
     return ((current_layer + 2 * padding - dilation * (kernel_size - 1) - 1) / stride) + 1
 
+
 def out_size_transpose(current_layer, padding, dilation, kernel_size, stride):
     return (current_layer - 1) * stride - 2 * padding + dilation * (kernel_size - 1) + padding + 1
+
 
 def reparameterization(mu, log_var: torch.Tensor, device):
     std = log_var.mul(0.5).exp_()
