3a82d6497f6243d6e4b5d029ad62890380ffa0a2
Ubuntu <ubuntu@ip-172-31-38-194.eu-west-2.compute.internal>
2020-03-24 21:13:11 +0000

    hard coded path to embeddings, created new embeddings

--- models/validation/linear.py
+++ models/validation/linear.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    embed_file = osp.join(osp.dirname(osp.dirname(osp.dirname(__file__))), "exports", "embeddings.json")
+    embed_file = osp.join("/home/ubuntu/Work/amino-acid-vae", "exports", "embeddings.json")
     json_embed = open(embed_file)
     json_data = json.load(json_embed)
 
