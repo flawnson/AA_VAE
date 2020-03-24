e5d254765c55dec2fa8681f45e5bfa41493cf81a
Ubuntu <ubuntu@ip-172-31-38-194.eu-west-2.compute.internal>
2020-03-24 15:08:35 +0000

    Added convolutional representation

--- models/convolutional_base_vae.py
+++ models/convolutional_base_vae.py
@@ -19,7 +19,7 @@ class Encoder(nn.Module):
 
         for n in range(layers):
             base_kernel_size = kernel_size + 1
-            block = ConvolutionalBlock(input_channels, output_channels, base_kernel_size,padded=True)
+            block = ConvolutionalBlock(input_channels, output_channels, base_kernel_size,padded=False)
             conv_layers.append(block)
             kernel_size = kernel_size * kernel_expansion_factor
             input_channels = output_channels
@@ -59,7 +59,7 @@ class Decoder(nn.Module):
         base_kernel_size = kernel_size
 
         for n in range(layers - 1):
-            block = ConvolutionalTransposeBlock(input_channels, output_channels, base_kernel_size,padded=True)
+            block = ConvolutionalTransposeBlock(input_channels, output_channels, base_kernel_size,padded=False)
             conv_layers.append(block)
             kernel_size = int(kernel_size / kernel_expansion_factor)
             input_channels = output_channels
@@ -137,7 +137,8 @@ class ConvolutionalBaseVAE(nn.Module):
         return z, mu, log_var
 
     def representation(self, x):
-        return self.bottleneck(self.encoder(x))[0]
+        x = self.encoder(self.embedding(x).transpose(1, 2))
+        return self.bottleneck((x))[1]
 
     def forward(self, x):
         h = self.encoder(self.embedding(x).transpose(1, 2))
