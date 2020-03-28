b8012c0cfcd39eceda9760017bd2db79b8e68df4
JyothishSoman <jyothish@juvenescence.ltd>
2020-03-28 01:00:28 +0000

    Running version of transformer

--- utils/logger.py
+++ utils/logger.py
@@ -3,4 +3,4 @@ import logging
 log = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s  %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s ',
                     datefmt='%m/%d/%Y %I:%M:%S %p',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
